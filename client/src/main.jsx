205a33540f988a9bf05fd600f5058369d8a8747e
Ronnie <ronniemartin0913@gmail.com>
2025-04-18 01:10:30 +0800

    feature: client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { ApolloProvider } from '@apollo/client';
 import client from './apollo-client';
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
